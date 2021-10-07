bf7614d68f2992475977eb6b2f03722dc1c191bd
Ivan Espinoza <icaro_19@hotmail.com>
2021-10-07 11:53:01 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -32,4 +32,9 @@ app.use("/api", require('./routes'))
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+// DEPLOY
+app.use(express.static(path.join(__dirname, "public")))
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
+
+
 module.exports = app;
