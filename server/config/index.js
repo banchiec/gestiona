3a6e891885ff84038c4c76c4dd4ad6df5b0f3c84
Ivan Espinoza <icaro_19@hotmail.com>
2021-10-07 12:39:08 +0200

    done

--- server/config/index.js
+++ server/config/index.js
@@ -35,5 +35,4 @@ module.exports = (app) => {
   app.use(express.static(path.join(__dirname, "..", "public")));
 
   // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
 };
